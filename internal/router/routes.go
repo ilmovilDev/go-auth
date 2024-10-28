1893bc7f538c8652a44d15dfca72b18c3ed8c254
Luis Carrasco <ilmovil.dev@gmail.com>
2024-10-28 09:30:25 -0300

    feat: Add environment configuration and database connection

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -13,5 +13,20 @@ func initializeRoutes(router *gin.Engine) {
 		auth.POST("/register", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{"message": "Register user"})
 		})
+		auth.POST("/authenticate", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Authenticate user"})
+		})
+		auth.POST("/forgot-password", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Forgot password"})
+		})
+		auth.POST("/update-password", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Update password"})
+		})
+		auth.GET("/confirm-email", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Confirm email"})
+		})
+		auth.GET("/confirm-token", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Confirm token"})
+		})
 	}
 }
